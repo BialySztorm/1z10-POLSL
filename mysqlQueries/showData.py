42e4e0087064703b56d9898a4a68b3e9c9ed3309
WhiteStorm <andrzejmmm1@gmail.com>
2024-03-09 20:57:08 +0100

    Add simple showing database content script
    Add script to display database content and table data
    
    Created functions to connect to a database, retrieve data from tables, show all data or specific table data interactively.

---
+++ mysqlQueries/showData.py
@@ -0,0 +1,87 @@
+import pymysql
+import os
+
+
+def connect_to_db():
+    # print('Getting credentials')
+    with open('.env') as f:
+        credentials = f.read().splitlines()
+    # print('Credentials', credentials)
+    host, port, user, passwd, db = credentials
+    connection = pymysql.connect(host=host,
+                                 port=int(port),
+                                 user=user,
+                                 password=passwd,
+                                 db=db,
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    return connection
+
+
+def get_data(table_name):
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    if (table_name):
+        cursor.execute(f"SELECT * FROM {table_name}")
+        rows = cursor.fetchall()
+        return rows
+    else:
+        cursor.execute("SHOW TABLES")
+        tables = cursor.fetchall()
+        table_data = {}
+        for table in tables:
+            table_name = table['Tables_in_defaultdb']
+            cursor.execute(f"SELECT * FROM {table_name}")
+            rows = cursor.fetchall()
+            table_data[table_name] = rows
+        return table_data
+
+
+def get_table_names():
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+    table_names = [table['Tables_in_defaultdb'] for table in tables]
+    table_names = {str(i + 1): table_name for i, table_name in enumerate(table_names)}
+    return table_names
+
+
+def show_data():
+    data = get_data(None)
+    for table_name, rows in data.items():
+        print(f"Table: {table_name}")
+        for row in rows:
+            print(row)
+        print()
+
+
+def show_table_data():
+    table_names = get_table_names()
+    print('Tables:', table_names)
+    option = input('Choose table number: ')
+    data = get_data(table_names[option])
+    for row in data:
+        print(row)
+
+
+def menu():
+    print('1. Show all data')
+    print('2. Show data for a specific table')
+    print('0. Exit')
+    choice = input('Enter choice: ')
+    if choice == '1':
+        show_data()
+    elif choice == '2':
+        show_table_data()
+    elif choice == '0':
+        exit()
+    else:
+        print('Invalid choice')
+    os.system('pause')
+    os.system('cls')
+
+
+if __name__ == '__main__':
+    while True:
+        menu()
