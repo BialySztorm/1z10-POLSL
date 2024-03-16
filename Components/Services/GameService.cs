96ee4c4f042f082ab6ab9549bdf77e4ba65d53ce
WhiteStorm <andrzejmmm1@gmail.com>
2024-03-16 21:38:43 +0100

    Fields initialized only in constructors can be made readonly CS-R1137
    Make fields initialized only in constructors readonly.
    
    Fields initialized only in constructors can be made readonly CS-R1137
    
    - Changed the MySqlConnection field to be readonly.

--- Components/Services/GameService.cs
+++ Components/Services/GameService.cs
@@ -18,7 +18,7 @@ internal class GameService
     private int _alivePlayers;
     private List<Tuple<int, string, string, string>> _questions = new List<Tuple<int, string, string, string>>();
     private int _currentQuestionIndex = 0;
-    private MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection();
+    private readonly MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection();
     private readonly IConfiguration _configuration;
 
     private bool SubstractLife(int index, int currentLives)
