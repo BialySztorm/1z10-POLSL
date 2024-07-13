ee5c38f1ac70d98627037e9391151f49d07de968
WhiteStorm <andrzejmmm1@gmail.com>
2024-07-13 16:54:04 +0200

    Quickfix add trycatch to connectino in highscore page
    Quickfix: Add try-catch block for database connection in highscore page
    
    Added a try-catch block to handle exceptions during database connection setup in the highscore page. This ensures proper error handling and prevents crashes due to connection issues.

--- Components/Pages/Highscores.razor.cs
+++ Components/Pages/Highscores.razor.cs
@@ -37,15 +37,26 @@ public partial class Highscores : ComponentBase
     protected override void OnInitialized()
     {
         MySqlConnection conn = GameServiceRef.GetConnection();
-        conn.Open();
-        string query = "SELECT player.first_name, player.last_name, player.age, game.date, highscore.highscore_value FROM highscore INNER JOIN player ON highscore.player_pk = player.player_pk INNER JOIN game ON player.game_pk  = game.game_pk;";
-
-        MySqlCommand cmd = new(query, conn);
-        using var reader = cmd.ExecuteReader();
-        while (reader.Read())
+        try
         {
-            _players.Add(new Player(reader.GetString(0), reader.GetString(1), reader.GetInt32(2).ToString(), reader.GetDateTime(3).ToString("yyyy-MM-dd"), reader.GetInt32(4).ToString()));
+            conn.Open();
+            string query = "SELECT player.first_name, player.last_name, player.age, game.date, highscore.highscore_value FROM highscore INNER JOIN player ON highscore.player_pk = player.player_pk INNER JOIN game ON player.game_pk  = game.game_pk;";
+
+            MySqlCommand cmd = new(query, conn);
+            using var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                _players.Add(new Player(reader.GetString(0), reader.GetString(1), reader.GetInt32(2).ToString(), reader.GetDateTime(3).ToString("yyyy-MM-dd"), reader.GetInt32(4).ToString()));
+            }
         }
-        conn.Close();
+        catch (MySql.Data.MySqlClient.MySqlException ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+        finally
+        {
+            conn.Close();
+        }
     }
 }
